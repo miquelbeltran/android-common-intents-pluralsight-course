39fa53571fac357bab5ba8d0ac3c11265cbf4ac4
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2019-05-06 14:30:16 +0200

    Starter Code

--- app/src/main/java/com/pluralsight/commonintents/ProfileActivity.kt
+++ app/src/main/java/com/pluralsight/commonintents/ProfileActivity.kt
@@ -1,16 +1,8 @@
 package com.pluralsight.commonintents
 
-import android.app.SearchManager
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.CalendarContract
-import android.provider.ContactsContract
-import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.actions.ReserveIntents
 import kotlinx.android.synthetic.main.activity_profile.*
-import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -56,117 +48,38 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun onClickContact() {
-        // Worth also explaining how to do the read contacts
-        // requires permission
-        val intent = Intent(Intent.ACTION_INSERT).apply {
-            type = ContactsContract.Contacts.CONTENT_TYPE
-            putExtra(ContactsContract.Intents.Insert.NAME, "Anne Droid")
-            putExtra(ContactsContract.Intents.Insert.EMAIL, "anne@example.com")
-        }
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
+        todo("Save as Contact")
     }
 
     private fun onClickEmail() {
-        val intent = Intent(Intent.ACTION_SENDTO).apply {
-            // only email apps should handle this
-            // if not it will be handled by sms app too
-            data = Uri.parse("mailto:anne@example.com")
-
-            // This won't work!
-//            putExtra(Intent.EXTRA_EMAIL, "anne@example.com")
-        }
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
+        todo("Send Email")
     }
 
     private fun onClickCall() {
-        // ACTION_CALL requires <uses-permission android:name="android.permission.CALL_PHONE" />
-
-        // ACTION_DIAL does not require permission
-
-        val intent = Intent(Intent.ACTION_DIAL).apply {
-            data = Uri.parse("tel:123456789")
-        }
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
+        todo("Call Friend")
     }
 
     private fun onClickWeb() {
-        // http or https required
-        val webpage: Uri = Uri.parse("http://example.com")
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
-
-        // Web urls can also be handled as deep links when receiving them
+        todo("Open Website")
     }
 
     private fun onClickListen() {
-        val artist = "Beethoven"
-        val intent = Intent(MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH).apply {
-
-            // Explain here the different Media focus types
-            putExtra(MediaStore.EXTRA_MEDIA_FOCUS, MediaStore.Audio.Artists.ENTRY_CONTENT_TYPE)
-            putExtra(MediaStore.EXTRA_MEDIA_ARTIST, artist)
-            putExtra(SearchManager.QUERY, artist)
-        }
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
-
+        todo("Play Music")
     }
 
     private fun omClickSearch() {
-        val intent = Intent(Intent.ACTION_WEB_SEARCH).apply {
-            putExtra(SearchManager.QUERY, "Gardening")
-        }
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
+        todo("Search Hobbies")
     }
 
     private fun onClickTaxi() {
-        // Also from "Basement" library
-        val intent = Intent(ReserveIntents.ACTION_RESERVE_TAXI_RESERVATION)
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
+        todo("Call a Taxi")
     }
 
     private fun onClickMaps() {
-        val address = "182 N. Union Ave, Farmington, UT 84025"
-        val intent = Intent(Intent.ACTION_VIEW).apply {
-            data = Uri.parse("geo:0,0?q=$address")
-        }
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
-
+        todo("View Location")
     }
 
     private fun onClickCalendar() {
-        val calendar = Calendar.getInstance()
-        calendar.set(Calendar.MONTH, Calendar.MARCH)
-        calendar.set(Calendar.DAY_OF_MONTH, 21)
-        val dateLong = calendar.timeInMillis
-
-        val intent = Intent(Intent.ACTION_INSERT).apply {
-            data = CalendarContract.Events.CONTENT_URI
-            putExtra(CalendarContract.Events.TITLE, "Anne Droid Birthday")
-//            putExtra(CalendarContract.Events.EVENT_LOCATION, location)
-            putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true)
-            putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, dateLong)
-            putExtra(CalendarContract.EXTRA_EVENT_END_TIME, dateLong)
-            putExtra(CalendarContract.Events.RRULE, "FREQ=YEARLY")
-
-        }
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
+        todo("Save Birthday")
     }
 }
